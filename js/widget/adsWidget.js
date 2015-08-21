58ba8b3989c87237b7cbe7acad18f20bbaf6bfc3
Ivan <dangelzm@gmail.com>
2015-08-21 11:36:27 +0300

    Add widget example

--- js/widget/adsWidget.js
+++ js/widget/adsWidget.js
@@ -1,18 +1,22 @@
 (function (OutsideJS) {
   "use strict";
-
-  console.log(OutsideJS.InitEnv);
+  if(OutsideJS.InitEnv && (OutsideJS.InitEnv.debug === true)){
+    console.log(OutsideJS.InitEnv);
+  }
 
   var params = OutsideJS.InitEnv.options;
+  if(params.key){
+    var adsHolder = document.querySelector('#ads-widget-' + params.key);
 
-  alert(params.text);
-
-  var adsHolder = document.querySelector('#ads-widget-' + params.key);
-
-  var div = document.createElement('div');
-  div.innerHTML = "<h1>Привет!</h1>";
-
-  adsHolder.appendChild(div);
-
+    if(adsHolder){
+      var div = document.createElement('div');
+      div.innerHTML = "<h1>Привет!</h1>";
+      adsHolder.appendChild(div);
+    } else {
+      console.warn('Element with id="%s" not found!!!', params.key);
+    }
+  } else {
+    console.warn('Widget key not specified!!!');
+  }
 
 })(window['OutsideJS']);
