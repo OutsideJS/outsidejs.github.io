d3ac8d283d225b60c2032131775baf1fc232e224
Ivan <dangelzm@gmail.com>
2015-08-21 15:49:41 +0300

    Add widget example

--- js/outside.js
+++ js/outside.js
@@ -10,6 +10,8 @@
 
     function init(scriptUrl, options, callback) {
         w[w['OutsideJsObject']].loadScript = loadScript;
+        w[w['OutsideJsObject']].loadCss = loadCss;
+        w[w['OutsideJsObject']].fetch = fetch;
         w[w['OutsideJsObject']].InitEnv = {
             options: options,
             baseUrl: scriptUrl.substring(0, scriptUrl.lastIndexOf("/") + 1)
@@ -36,6 +38,55 @@
                 script.onreadystatechange = null;
             }
         }
+    }
+
+    function loadCss(url) {
+        var fileref = document.createElement("link");
+        fileref.setAttribute("rel", "stylesheet");
+        fileref.setAttribute("type", "text/css");
+        fileref.setAttribute("href", url);
+        document.getElementsByTagName("head")[0].appendChild(fileref);
+    }
+
+    function fetch(url, postData, successCallback, errorCallback, alwaysCallback) {
+        var req = createXMLHTTPObject();
+        if (!req) return;
+        var method = (postData) ? "POST" : "GET";
+        req.open(method,url,true);
+        if (postData)
+            req.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+        req.onreadystatechange = function () {
+            alwaysCallback && alwaysCallback(req);
+            if (req.readyState != 4) return;
+            if (req.status != 200 && req.status != 304) {
+                errorCallback && errorCallback(req.status, req);
+                return;
+            }
+            successCallback && successCallback(req.response, req);
+        };
+        if (req.readyState == 4) return;
+        req.send(postData);
+    }
+
+    var XMLHttpFactories = [
+        function () {return new XMLHttpRequest()},
+        function () {return new ActiveXObject("Msxml2.XMLHTTP")},
+        function () {return new ActiveXObject("Msxml3.XMLHTTP")},
+        function () {return new ActiveXObject("Microsoft.XMLHTTP")}
+    ];
+
+    function createXMLHTTPObject() {
+        var xmlhttp = false;
+        for (var i=0;i<XMLHttpFactories.length;i++) {
+            try {
+                xmlhttp = XMLHttpFactories[i]();
+            }
+            catch (e) {
+                continue;
+            }
+            break;
+        }
+        return xmlhttp;
     }
 
     w[w['OutsideJsObject']] = function () {
--- js/widget/adsWidget.js
+++ js/widget/adsWidget.js
@@ -6,22 +6,28 @@
 
   var params = OutsideJS.InitEnv.options;
   if(params.key){
-    var adsHolder = document.querySelector('#ads-widget-' + params.key);
 
-    if(adsHolder){
-      var div = document.createElement('div');
-      div.innerHTML = '' +
-        '<style>' +
-        '#ads-widget-'+params.key+' {' +
-        ' -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box;' +
-        ' border:1px solid #369; border-radius: 6px; padding: 5px; height: 150px;' +
-        '}' +
-        ' #ads-widget-'+params.key+' .widget-header-'+params.key+'{' +
-        '   color: red;' +
-        '}' +
-        '</style>' +
-        '<div class="widget-header-'+params.key+'">Привет!</div>';
-      adsHolder.appendChild(div);
+    var $holder = document.querySelector('#ads-widget-' + params.key);
+    if($holder){
+      OutsideJS.loadCss('./js/widget/products.css');
+      OutsideJS.fetch('./js/widget/data.json', null, function(data){
+        var response = JSON.parse(data);
+        for(var i = 0; i < response.products.length; i++) {
+          var item = response.products[i];
+          var post = document.createElement('div');
+          post.innerHTML = [
+            '<div class="product">',
+            ' <div class="title"><a href="'+item.href+'">'+ item.title +'</a></div>',
+            ' <div class="image"><a href="'+item.href+'"><img src="'+item.img+'" /></a></div>',
+            '</div>'
+          ].join('');
+          $holder.appendChild(post);
+        }
+      }, function(status, req){
+        console.log(status);
+        console.log(req);
+      });
+
     } else {
       console.warn('Element with id="%s" not found!!!', params.key);
     }
